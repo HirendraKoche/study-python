499c077a8fb7922c8b36e9f53f3f88c5a32e1000
HirendraKoche <hirendrakoche1@outlook.com>
2024-03-30 23:54:05 +0530

    Added funtion to get triangle perimeter
    Updated main function get triangle sides.

--- operator_ex1.py
+++ operator_ex1.py
@@ -5,14 +5,26 @@
 def get_areaoftriangle(base, height):
     return 0.5 * base * height
 
+def get_perimeteroftriangle(side_a, side_b, side_c):
+    return side_a + side_b + side_c
+
 def main():
     v_base = input("Base: ")
     v_height = input("Height: ")
+    v_side_a = input("Side A length: ")
+    v_side_b = input("Side B length: ")
+    v_side_c = input("Side C length: ")
     try:
-        v_base = float(v_base)
-        v_height = float(v_height)
+        v_base, v_height = float(v_base), float(v_height)
+        v_side_a, v_side_b, v_side_c = int(v_side_a), int(v_side_b), int(v_side_c)
         print("Area of Triangle =", get_areaoftriangle(v_base, v_height))
-    except ValueError:
-        print(v_base, "or", v_height, "is not number or deciaml")
+        print("Perimeter of Triangle =", get_perimeteroftriangle(v_side_a, v_side_b, v_side_c))
+    except ValueError:  
+        print("One of the provided inputs is not number or deciaml")
 
-main()
+try:
+    main()
+except KeyboardInterrupt:
+    print("\nUser terminated execution")
+finally:
+    print("Good Bye!")
